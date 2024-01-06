91541e9e0526de31349e292496cd4c8cc20ba423
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2024-01-06 10:38:46 +0100

    feat: add social links to Settings

--- src/payload/globals/Settings/tabs/Links.ts
+++ src/payload/globals/Settings/tabs/Links.ts
@@ -1,6 +1,52 @@
-import type { Tab } from 'payload/types'
+import type { Field, Option, Tab } from 'payload/types'
 
 import link from '../../../fields/link'
+import deepMerge from '../../../utilities/deepMerge'
+
+interface LinksArrayParams {
+  name: string
+  label: Record<string, string> | string
+  options: Option[]
+  overrides?: Record<string, unknown>
+}
+
+function linksArray(params: LinksArrayParams): Field {
+  const field: Field = {
+    type: 'array',
+    name: params.name,
+    label: params.label,
+    fields: [
+      {
+        type: 'row',
+        fields: [
+          {
+            name: 'type',
+            type: 'select',
+            label: {
+              en: 'Type',
+              it: 'Tipo',
+            },
+            required: true,
+            options: params.options,
+          },
+          link({
+            overrides: {
+              admin: {
+                condition: (_: Partial<any>, siblingData: Partial<any>) => siblingData?.type,
+              },
+              required: true,
+            },
+            disableHeading: true,
+            appearances: false,
+            disableLabel: true,
+          }),
+        ],
+      },
+    ],
+  }
+
+  return deepMerge(field, params.overrides)
+}
 
 export const linksTab: Tab = {
   label: {
@@ -8,63 +54,77 @@ export const linksTab: Tab = {
     it: 'Links',
   },
   fields: [
-    {
-      type: 'array',
+    linksArray({
       name: 'legalLinks',
       label: {
         en: 'Legal Links',
         it: 'Link Legali',
       },
-      fields: [
+      options: [
         {
-          type: 'row',
-          fields: [
-            {
-              name: 'type',
-              type: 'select',
-              label: {
-                en: 'Type',
-                it: 'Tipo',
-              },
-              required: true,
-              options: [
-                {
-                  label: {
-                    en: 'Privacy Policy',
-                    it: 'Privacy Policy',
-                  },
-                  value: 'privacy',
-                },
-                {
-                  label: {
-                    en: 'Cookie Policy',
-                    it: 'Cookie Policy',
-                  },
-                  value: 'cookie',
-                },
-                {
-                  label: {
-                    en: 'Terms & Conditions',
-                    it: 'Termini & Condizioni',
-                  },
-                  value: 'terms',
-                },
-              ],
-            },
-            link({
-              overrides: {
-                admin: {
-                  condition: (_: Partial<any>, siblingData: Partial<any>) => siblingData?.type,
-                },
-                required: true,
-              },
-              disableHeading: true,
-              appearances: false,
-              disableLabel: true,
-            }),
-          ],
+          label: {
+            en: 'Privacy Policy',
+            it: 'Privacy Policy',
+          },
+          value: 'privacy',
+        },
+        {
+          label: {
+            en: 'Cookie Policy',
+            it: 'Cookie Policy',
+          },
+          value: 'cookie',
+        },
+        {
+          label: {
+            en: 'Terms & Conditions',
+            it: 'Termini & Condizioni',
+          },
+          value: 'terms',
         },
       ],
-    },
+    }),
+    linksArray({
+      name: 'socialLinks',
+      label: {
+        en: 'Social Links',
+        it: 'Link Social',
+      },
+      options: [
+        {
+          label: 'X / Twitter',
+          value: 'x',
+        },
+        {
+          label: 'Instagram',
+          value: 'instagram',
+        },
+        {
+          label: 'Facebook',
+          value: 'facebook',
+        },
+        {
+          label: 'YouTube',
+          value: 'youtube',
+        },
+        {
+          label: 'Vimeo',
+          value: 'vimeo',
+        },
+        {
+          label: 'TikTok',
+          value: 'tiktok',
+        },
+        {
+          label: 'Pinterest',
+          value: 'pinterest',
+        },
+      ],
+      overrides: {
+        admin: {
+          className: 'divider-top',
+        },
+      },
+    }),
   ],
 }
